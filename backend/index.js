e5012141b0e96a9ea31d4151a558601ac394f061
vitviki <vtyagi1991@gmail.com>
2024-03-21 22:32:04 +0530

    added CORS for all incoming requests

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import cors from "cors";
 import booksRouter from "./routes/booksRoutes.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 dotenv.config();
 
